581e0e0296e59ab968598a68279ede4edc9050a8
dcamriff <driffelATL@gmail.com>
2017-11-21 13:42:58 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "jack";
+var cardFour = "ace";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
