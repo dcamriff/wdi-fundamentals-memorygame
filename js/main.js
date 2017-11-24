de8d3056b8bdd9e46a12ffea9f5f9d8e8dfdad9b
dcamriff <driffelATL@gmail.com>
2017-11-24 14:46:03 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+/*
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+*/
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamnds",
+	cardImage: "images/king-of-diamonds.png",
+}
+]
 var cardsInPlay = [];
-
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
@@ -11,14 +33,16 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);
-	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
 	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!');
 	} else {
 		alert('Sorry, try again.');
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 	}
 };
 flipCard(0);
 flipCard(2);
+
